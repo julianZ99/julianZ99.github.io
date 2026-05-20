55544dc8b1f2a6f596e62169595cd465585ed93d
Julian Zapata <zapatajulian79@gmail.com>
2026-05-20 13:43:50 -0300

    feat: update SITE_TITLE and modify page titles for consistency

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Projects';
+export const SITE_TITLE = 'julianZ99';
 export const SITE_DESCRIPTION = 'My personal project portfolio';
